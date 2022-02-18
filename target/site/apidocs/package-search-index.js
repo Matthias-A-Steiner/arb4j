522c13d4fded174e600b15ea527da592914df844
Stephen Crowley <stephencrowley214@gmail.com>
2022-02-17 22:09:23 -0600

    remove generated docs

--- target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"arblib"},{"l":"arblib.functions"},{"l":"arblib.graphing"},{"l":"arblib.graphing.plots"},{"l":"util"}];updateSearchResults();
