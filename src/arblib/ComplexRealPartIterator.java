f1e1d15a871dfac13c2f51b1c1d742e6b34d6ed2
Stephen Crowley <stephencrowley214@gmail.com>
2022-03-21 19:30:12 -0500

    classes for iterating over the real and imaginary parts of a Complex (vector)

---
+++ src/arblib/ComplexRealPartIterator.java
@@ -0,0 +1,19 @@
+package arblib;
+
+import java.util.Iterator;
+
+public class ComplexRealPartIterator extends ComplexPartIterator 
+{
+
+  public ComplexRealPartIterator(Complex x)
+  {
+    this.x = x;
+  }
+
+  @Override
+  public Real next()
+  {
+    return x.get(i++).getReal();
+  }
+
+}
