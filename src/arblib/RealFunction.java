5a18f3f518c64633a84b5a45fd76956fffd103b5
Stephen Crowley <stephencrowley214@gmail.com>
2022-03-25 19:39:34 -0500

    document

--- src/arblib/RealFunction.java
+++ src/arblib/RealFunction.java
@@ -20,15 +20,58 @@ public interface RealFunction
   public void evaluate(Real res, int order, int prec, Real z);
 
   /**
-   * TODO: rectify the SWIGTYPE prefixes
+   * <code>
+   * Rigorously isolates single roots of a real analytic function on the interior of an interval.
    * 
-   * @param found
-   * @param flags
-   * @param interval
-   * @param maxdepth
-   * @param maxeval
-   * @param maxfound
-   * @param prec
+   * This routine writes an array of n interesting subintervals of interval to found and corresponding flags to flags, returning the integer n. 
+   * 
+   * The output has the following properties: 
+   * 
+   * 1. The function has no roots on interval outside of the output subintervals.
+   * 
+   * 2. Subintervals are sorted in increasing order (with no overlap except possibly starting and ending with the same point).
+   * 
+   * 3. Subintervals with a flag of 1 contain exactly one (single) root.
+   * 
+   * 4. Subintervals with any other flag may or may not contain roots.
+   * 
+   * 5. If no flags other than 1 occur, all roots of the function on interval have been isolated.
+   *  
+   * If there are output subintervals on which the existence or nonexistence of roots could not be determined, 
+   * further searches on those subintervals may be attempted with possibly with increased precision and/or increased bounds for 
+   * the breaking criteria. 
+   * 
+   * Note that roots of multiplicity higher than one and roots located exactly at endpoints cannot be isolated by the this function.
+   
+   * The following breaking criteria are implemented:
+   * At most maxdepth recursive subdivisions are attempted. 
+   * The smallest details that can be distinguished are therefore about times the width of interval. 
+   * 
+   * The total number of calls to func is thereby restricted to a small multiple of maxeval (the actual count can be slightly higher depending on implementation details). 
+   * </code>
+   * 
+   * Warning: it is assumed that subdivision points of interval can be represented
+   * exactly as floating-point numbers in memory. Do not pass as input.
+   * 1+/-2^(-10^(100))
+   * 
+   * @param found    upon return this is populated with an array of n interesting
+   *                 subintervals of interval
+   * @param flags    flags corresponding to the found subintervals
+   * @param interval the region with which to isolate the roots
+   * @param maxdepth A typical, reasonable value might be between 20 and 50.
+   * @param maxeval  If the total number of tested subintervals exceeds maxeval,
+   *                 the algorithm is terminated and any untested subintervals are
+   *                 added to the output. A typical, reasonable value might be
+   *                 between 100 and 100000.
+   * @param maxfound The algorithm terminates if maxfound roots have been
+   *                 isolated. In particular, setting maxfound to 1 can be used to
+   *                 locate just one root of the function even if there are
+   *                 numerous roots. To try to find all roots, LONG_MAX may be
+   *                 passed.
+   * @param prec     denotes the precision used to evaluate the function. It is
+   *                 possibly also used for some other arithmetic operations
+   *                 performed internally by the algorithm. Note that it probably
+   *                 does not make sense for maxdepth to exceed prec.
    * @return
    */
   public default int isolateRoots(SWIGTYPE_p_arf_interval_ptr found,
