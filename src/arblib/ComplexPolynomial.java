148a282470f07392e4ca67a7443f08848f79f6ec
Stephen Crowley <stephencrowley214@gmail.com>
2022-03-28 22:06:11 -0500

    /**    * @see arblib#acb_poly_product_roots(ComplexPolynomial, Complex, int, int)    *    * @param xs    * @param prec    * @return    */   public ComplexPolynomial polyRoots(Complex xs, int prec)   {     arblib.acb_poly_product_roots(this, xs, xs.dim, prec);     return this;   }

--- src/arblib/ComplexPolynomial.java
+++ src/arblib/ComplexPolynomial.java
@@ -10,22 +10,30 @@ package arblib;
 
 import static arblib.Constants.*;
 
-public class ComplexPolynomial implements AutoCloseable,ComplexFunction {
-  private transient long swigCPtr;
+public class ComplexPolynomial implements
+                               AutoCloseable,
+                               ComplexFunction
+{
+  private transient long      swigCPtr;
   protected transient boolean swigCMemOwn;
 
-  public ComplexPolynomial(long cPtr, boolean cMemoryOwn) {
+  public ComplexPolynomial(long cPtr, boolean cMemoryOwn)
+  {
     swigCMemOwn = cMemoryOwn;
-    swigCPtr = cPtr;
+    swigCPtr    = cPtr;
   }
 
-  public static long getCPtr(ComplexPolynomial obj) {
+  public static long getCPtr(ComplexPolynomial obj)
+  {
     return (obj == null) ? 0 : obj.swigCPtr;
   }
 
-  public synchronized void delete() {
-    if (swigCPtr != 0) {
-      if (swigCMemOwn) {
+  public synchronized void delete()
+  {
+    if (swigCPtr != 0)
+    {
+      if (swigCMemOwn)
+      {
         swigCMemOwn = false;
         arblibJNI.delete_ComplexPolynomial(swigCPtr);
       }
@@ -33,13 +41,12 @@ public class ComplexPolynomial implements AutoCloseable,ComplexFunction {
     }
   }
 
-
   @Override
   public void close()
-  { 
-      delete();
+  {
+    delete();
   }
-  
+
   /**
    * TODO: change method signature so it takes the order and precision parameters
    */
@@ -48,36 +55,56 @@ public class ComplexPolynomial implements AutoCloseable,ComplexFunction {
   {
     arblib.acb_poly_evaluate(w, this, z, Complex.defaultPrec);
     return w;
-  } 
-  
+  }
 
-  public void setCoeffs(Complex value) {
+  public void setCoeffs(Complex value)
+  {
     arblibJNI.ComplexPolynomial_coeffs_set(swigCPtr, this, Complex.getCPtr(value), value);
   }
 
-  public Complex getCoeffs() {
+  public Complex getCoeffs()
+  {
     long cPtr = arblibJNI.ComplexPolynomial_coeffs_get(swigCPtr, this);
-    return (cPtr == 0) ? null : new Complex(cPtr, false);
+    return (cPtr == 0) ? null : new Complex(cPtr,
+                                            false);
   }
 
-  public void setLength(int value) {
+  public void setLength(int value)
+  {
     arblibJNI.ComplexPolynomial_length_set(swigCPtr, this, value);
   }
 
-  public int getLength() {
+  public int getLength()
+  {
     return arblibJNI.ComplexPolynomial_length_get(swigCPtr, this);
   }
 
-  public void setAlloc(int value) {
+  public void setAlloc(int value)
+  {
     arblibJNI.ComplexPolynomial_alloc_set(swigCPtr, this, value);
   }
 
-  public int getAlloc() {
+  public int getAlloc()
+  {
     return arblibJNI.ComplexPolynomial_alloc_get(swigCPtr, this);
   }
 
-  public ComplexPolynomial() {
-    this(arblibJNI.new_ComplexPolynomial(), true);
+  public ComplexPolynomial()
+  {
+    this(arblibJNI.new_ComplexPolynomial(),
+         true);
   }
 
+  /**
+   * @see arblib#acb_poly_product_roots(ComplexPolynomial, Complex, int, int)
+   * 
+   * @param xs
+   * @param prec
+   * @return
+   */
+  public ComplexPolynomial polyRoots(Complex xs, int prec)
+  {
+    arblib.acb_poly_product_roots(this, xs, xs.dim, prec);
+    return this;
+  }
 }
