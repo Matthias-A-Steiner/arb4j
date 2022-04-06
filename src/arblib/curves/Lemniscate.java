3f388a1fe04394cf1581e7a082ee8da278650663
Sir Isaac Newton <isaac@ancient.hist.ory>
2022-04-05 22:42:06 -0500

    TODO: arclength lemniscate parameterization

---
+++ src/arblib/curves/Lemniscate.java
@@ -0,0 +1,17 @@
+package arblib.curves;
+
+import arblib.Complex;
+import arblib.ComplexFunction;
+
+public class Lemniscate implements
+                        ComplexFunction
+{
+
+  @Override
+  public Complex evaluate(Complex z, int order, int prec, Complex w)
+  {
+//  return (sqrt(2)*cos(t))/(1-i*sin(t))
+    return null;
+  }
+
+}
