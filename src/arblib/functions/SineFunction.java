cf7728d57a234cd334707eef168ffe8cdf2bb324
Stephen Crowley <stephencrowley214@gmail.com>
2022-04-05 23:16:41 -0500

    2do

--- src/arblib/functions/SineFunction.java
+++ src/arblib/functions/SineFunction.java
@@ -10,35 +10,12 @@ public class SineFunction implements
   @Override
   public Real evaluate(Real z, int order, int prec, Real w)
   {
-    try
-    {
-      assert z != null;
-      assert w != null;
-      long zPtr = z.swigCPtr;
-      // new Exception().printStackTrace();
-      // System.out.format( "z pointer equals 0x%x\n", zPtr );
-      long wPtr = w.swigCPtr;
-      // System.out.format( "w pointer equals 0x%x\n", wPtr);
-      // System.out.flush();
-//       System.out.flush();
-//       System.out.format( "z=%s\n", z.doubleValue());
-//       System.out.flush();
-
-      // TODO: implement order>1
-      assert order == 1;
-      // System.out.format( "THe z addy is 0x%x", z.swigCPtr );
-      Real sin = z.sin(prec, w);
-//      System.out.format( "w=%s\n", w);
-//      System.out.flush();
-      return sin;
-    }
-    catch (Throwable t)
-    {
-      t.printStackTrace();
-      System.out.flush();
-      System.exit(1);
-    }
-    return w;
+    assert z != null;
+    assert w != null;
+    // TODO: implement derivative(s) with order>1
+    assert order == 1;
+    Real sin = z.sin(prec, w);
+    return sin;
   }
 
 }
