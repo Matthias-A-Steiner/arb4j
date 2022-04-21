510d32084a08f5176be6f6837132ff3c4c11a44d
George Carlin <the@great.beyond>
2022-04-20 23:32:25 -0500

    remove cuss words now that the shit is fucking working like its fucking supposed to!

--- src/arblib/RealFunctionTest.java
+++ src/arblib/RealFunctionTest.java
@@ -4,21 +4,12 @@ import org.junit.Test;
 
 import arblib.functions.SineFunction;
 import arblib.functions.ZFunction;
-import junit.framework.TestCase;
 
 public class RealFunctionTest
 {
 
-//  @Test
-//  public void testLocateRoots3()
-//  {
-//    shitcunt();
-////    System.out.println("roots=" + roots);
-////    roots.refine(sineFunction, 55);
-//
-//  }
-
-  public void shit()
+  @Test
+  public void testLocateRootsSine()
   {
     SineFunction sineFunction = new SineFunction();
     FoundRoots   roots        = sineFunction.locateRoots(new RealRootInterval(3,
@@ -30,24 +21,16 @@ public class RealFunctionTest
   }
 
   @Test
-  public void testLocateRoots4()
-  {
-    // fuck();
-    shit();
-    fuck();
-//    roots.refine(f, 25);
-
-  }
-
-  public void fuck()
+  public void testLocateRootsHardyZ()
   {
     RealFunction     f     = new RealPart(new ZFunction());
     RealRootInterval damn  = new RealRootInterval(14.13,
                                                   14.15);
     FoundRoots       roots = f.locateRoots(damn, 30, 50, 1, 256);
     System.out.println("roots=" + roots);
-    
-    //roo
-     roots.refine(f, 20);
+
+    roots.refine(f, 20);
+
   }
+
 }
